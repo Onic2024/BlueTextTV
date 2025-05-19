56633ffb056854a23632af1f58abb00b6bc63a16
Onic2024 <robipoc2024@gmail.com>
2025-05-19 20:59:06 +0700

    Update BlueTyping.ino

--- BlueTextTV.ino
+++ BlueTextTV.ino
@@ -4,14 +4,16 @@
 #include <BLE2902.h>
 #include "USB.h"
 #include "USBHIDKeyboard.h"
+#include "USBHIDMouse.h"
 
 #define SERVICE_UUID        "12345678-1234-5678-1234-56789abcdef0"
 #define CHARACTERISTIC_UUID "abcdef12-3456-7890-abcd-ef1234567890"
 
+USBHIDMouse Mouse;
 USBHIDKeyboard Keyboard;
 BLECharacteristic* pCharacteristic;
 
-// BLE Disconnect Callback
+// === BLE Disconnect Callback ===
 class MyServerCallbacks : public BLEServerCallbacks {
   void onDisconnect(BLEServer* pServer) {
     Serial.println("BLE disconnected, restarting advertising...");
@@ -20,7 +22,7 @@ class MyServerCallbacks : public BLEServerCallbacks {
   }
 };
 
-// BLE Write Callback
+// === BLE Write Callback ===
 class MyCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic* pChar) {
     String rx = String(pChar->getValue().c_str());
@@ -32,7 +34,7 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       Keyboard.write(KEY_BACKSPACE);
     }
     else if (rx == "\n") {
-      Keyboard.write(KEY_RETURN);  
+      Keyboard.write(KEY_RETURN);
     }
     else if (rx == "[UP]") {
       Keyboard.write(KEY_UP_ARROW);
@@ -47,10 +49,26 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       Keyboard.write(KEY_RIGHT_ARROW);
     }
     else if (rx == "[ESC][WIN]") {
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press(KEY_ESC);
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.press(KEY_LEFT_GUI);  // Tombol Windows (GUI)
+      Keyboard.press(KEY_ESC);       // Tombol ESC
+      delay(100);                    // Delay kecil untuk memastikan terkirim
+      Keyboard.releaseAll();         // Lepaskan semua tombol
+    }
+    else if (rx.startsWith("[MOUSE_MOVE:")) {
+      int x = 0, y = 0;
+      sscanf(rx.c_str(), "[MOUSE_MOVE:%d,%d]", &x, &y);
+      Mouse.move(x, y, 0);  // Tambah scroll = 0 (parameter ketiga wajib)
+    }
+    else if (rx == "[MOUSE_CLICK]") {
+      Mouse.click(MOUSE_LEFT);
+    }
+    else if (rx == "[MOUSE_RIGHT]") {
+      Mouse.click(MOUSE_RIGHT);
+    }
+    else if (rx.startsWith("[MOUSE_SCROLL:")) {
+      int scrollY = 0;
+      sscanf(rx.c_str(), "[MOUSE_SCROLL:%d]", &scrollY);
+      Mouse.move(0, scrollY);
     }
     else {
       Keyboard.print(rx);
@@ -61,13 +79,14 @@ class MyCallbacks : public BLECharacteristicCallbacks {
 void setup() {
   Serial.begin(115200);
   delay(500);
-
+  
+  Mouse.begin();
   Keyboard.begin();
   delay(3000);  
 
-  BLEDevice::init("BlueTextTV");
+  BLEDevice::init("BlueTyping");
   BLEServer* pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  pServer->setCallbacks(new MyServerCallbacks()); 
 
   BLEService* pService = pServer->createService(SERVICE_UUID);
 
